0271cf4b6b6d00f2a7fd0ae9738e8c10dc84f367
James Volmert <james.volmert@gmail.com>
2016-05-03 17:14:19 -0600

    finished the restaurants tab

--- www/views/restaurants/restaurantsCtrl.js
+++ www/views/restaurants/restaurantsCtrl.js
@@ -1,4 +1,24 @@
 angular.module('myApp')
-.controller('restaurantsCtrl', function($scope) {
+.controller('restaurantsCtrl', function($scope, $http) {
 
+  $scope.page = 0;
+  $scope.total = 1;
+  $scope.restaurants = [];
+
+  $scope.getRestaurants = function() {
+    $scope.page++;
+    $http.get('https://ionic-in-action-api.herokuapp.com/restaurants?age=' + $scope.page).success(function (response) {
+      angular.forEach(response.restaurants, function (restaurant) {
+        $scope.restaurants.push(restaurant);
+      });
+
+      $scope.total = response.totalPages;
+      console.log('this is a response',response);
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    }).error(function (err) {
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+      console.log(err);
+    });
+  };
+  $scope.getRestaurants();
 });
